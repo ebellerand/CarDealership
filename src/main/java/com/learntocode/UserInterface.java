86e9b4d23ce9c1d08768d91e43526a90ac30a3fc
ebellerand <ebellerand@appdev.yearup.org>
2023-05-14 18:05:24 -0400

    Added CSV read/write functionality

--- src/main/java/com/learntocode/UserInterface.java
+++ src/main/java/com/learntocode/UserInterface.java
@@ -1,4 +1,12 @@
 package com.learntocode;
 
 public class UserInterface {
+    private Dealership dealership;
+    public void display() {
+        init();
+    }
+
+    private void init() {
+        dealership = new Dealership(dealership.getName(), dealership.getAddress(), dealership.getPhone(), dealership.getInventory());
+    }
 }
